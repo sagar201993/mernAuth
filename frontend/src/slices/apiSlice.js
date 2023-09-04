24894fdaa5d445af3fe2b51d2a101128cd0a5c89
munishsagar <sagarvirk1@gmail.com>
2023-09-04 19:19:56 +0200

    first commit

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -1,7 +1,7 @@
 import { fetchBaseQuery, createApi } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "",
+  baseUrl: "https://mernauth-aopf.onrender.com",
 });
 
 export const apiSlice = createApi({
